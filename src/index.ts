8ee57cb10f26ec9590f6b008789bcf1957db080f
ngdangkhanhdev <ngdangkhanh233@gmail.com>
2026-05-17 21:48:56 +0700

    Test set up

--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,12 @@
-const name: string = 'Nguyễn Đăng Khánh'
-console.log(name)
+import express, { Express, Request, Response } from 'express'
+
+const app: Express = express()
+const port: number = 3000
+
+app.get('/', (req: Request, res: Response) => {
+  res.send('Trang chủ')
+})
+
+app.listen(port, () => {
+  console.log(`App listening on port ${port}`)
+})
